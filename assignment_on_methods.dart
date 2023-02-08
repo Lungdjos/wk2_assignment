c11181c06f0c4e62794c59ef0cc05997f7777c53
Lungdjos <lungudjos@gmail.com>
2023-02-08 16:49:46 +0200

    Adding to repository the assignment files

---
+++ assignment_on_methods.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {}
+
+// the method that adds the two numbers
+double addTwo(int num1, double num2) {
+  double result = num1 + num2;
+  return result;
+}
+
+// the method that subtracts the two numbers
+int subtractTwo(dynamic num1, dynamic num2) {
+  int result = num1 - num2;
+  return result;
+}
+
+// the method that multiplies the two numbers
+
